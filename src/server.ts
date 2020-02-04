8e4c7cdbd55901e126a313e2212e0787229298eb
Emil Mattsson <emil.mattsson@ist.com>
2020-02-04 12:03:37 +0100

    refactoring and adding node types

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import express, { Express, Request, Response, NextFunction } from 'express'
+import express, { Express, Request, Response, NextFunction, RequestHandler } from 'express'
 import morgan from 'morgan'
 
 import asyncRequestWrapper from '../middleware/asyncRequestWrapper'
 
 const app: Express = express()
+const logger: RequestHandler = morgan('dev')
 const port: Number = Number(process.env.PORT) || 4000
 
-// Logger middleware -> "tiny" means tiny logging output
-app.use(morgan('tiny'))
+app.use(logger)
 
 app.get(
   '/',
